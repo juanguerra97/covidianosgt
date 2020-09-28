1b277cf544eba9e235e06ea6ce7444a298ccf7ae
Juan Guerra <juanguerra97@protonmail.com>
2020-09-27 20:10:55 -0600

    controlador para endpoint  de casos de covid

---
+++ Controllers/CasosCovidController.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace covidiano.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CasosCovidController
+    {
+        
+        [HttpGet]
+        public async Task<ActionResult<List<string>>> GetAll()
+        {
+            return new List<string> {
+                "Xela",
+                "Chiquimula",
+                "Tiqui"
+            };
+        }
+    }
+}
